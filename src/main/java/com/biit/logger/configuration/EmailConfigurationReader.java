7364f23d02aa9c3b485be1216330ea693750f79a
Jorge Izquierdo Ciges <jorizci@gmail.com>
2015-02-20 15:28:09 +0100

    Updated biit logger to use multiple configuration files.

--- src/main/java/com/biit/logger/configuration/EmailConfigurationReader.java
+++ src/main/java/com/biit/logger/configuration/EmailConfigurationReader.java
@@ -1,42 +1,50 @@
 package com.biit.logger.configuration;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 import com.biit.logger.BiitLogger;
-import com.biit.utils.file.PropertiesFile;
+import com.biit.utils.configuration.ConfigurationReader;
+import com.biit.utils.configuration.PropertiesSourceFile;
+import com.biit.utils.configuration.SystemVariablePropertiesSourceFile;
+import com.biit.utils.configuration.exception.PropertyNotFoundException;
 
-/**
- * Jar applications cannot have configuration files inside JAR file, must be in an external folder to allow editing.
- */
-public class EmailConfigurationReader {
-	private final String CONFIG_FILE = "settings.conf";
+public class EmailConfigurationReader extends ConfigurationReader {
 
-	private final String EMAIL_ENABLED = "mail.enabled";
-	private final String EMAIL_TO = "mail.to";
-	private final String EMAIL_SMTP_SERVER_TAG = "mail.smtpserver";
-	private final String EMAIL_USERNAME_TAG = "mail.username";
-	private final String EMAIL_PASSWORD_TAG = "mail.password";
-	private final String EMAIL_SENDER_TAG = "mail.sender";
+	private static final String DATABASE_CONFIG_FILE = "settings.conf";
+	private static final String BIIT_LOG_SYSTEM_VARIABLE_CONFIG = "BIIT_LOG_CONFIG";
 
+	private final String ID_EMAIL_ENABLED = "mail.enabled";
+	private final String ID_EMAIL_TO = "mail.to";
+	private final String ID_EMAIL_SMTP_SERVER_TAG = "mail.smtpserver";
+	private final String ID_EMAIL_USERNAME = "mail.username";
+	private final String ID_EMAIL_PASSWORD = "mail.password";
+	private final String ID_EMAIL_SENDER = "mail.sender";
+
+	private final String DEFAULT_EMAIL_ENABLED = "false";
+	private final String DEFAULT_EMAIL_TO = null;
 	private final String DEFAULT_EMAIL_SMTP_SERVER = "smtp.mail.com";
 	private final String DEFAULT_EMAIL_USERNAME = "noreply@email.com";
 	private final String DEFAULT_EMAIL_PASSWORD = "password";
 	private final String DEFAULT_EMAIL_SENDER = "BiiT Sourcing Solutions";
 
-	private boolean emailEnabled = false;
-	private String smtpServer;
-	private String emailUser;
-	private String emailPassword;
-	private String emailSender;
-	private List<String> emailToList;
-
 	private static EmailConfigurationReader instance;
 
 	private EmailConfigurationReader() {
-		readConfig();
+		super();
+
+		addProperty(ID_EMAIL_ENABLED, DEFAULT_EMAIL_ENABLED);
+		addProperty(ID_EMAIL_TO, DEFAULT_EMAIL_TO);
+		addProperty(ID_EMAIL_SMTP_SERVER_TAG, DEFAULT_EMAIL_SMTP_SERVER);
+		addProperty(ID_EMAIL_USERNAME, DEFAULT_EMAIL_USERNAME);
+		addProperty(ID_EMAIL_PASSWORD, DEFAULT_EMAIL_PASSWORD);
+		addProperty(ID_EMAIL_SENDER, DEFAULT_EMAIL_SENDER);
+
+		addPropertiesSource(new PropertiesSourceFile(DATABASE_CONFIG_FILE));
+		addPropertiesSource(new SystemVariablePropertiesSourceFile(BIIT_LOG_SYSTEM_VARIABLE_CONFIG,
+				DATABASE_CONFIG_FILE));
+
+		readConfigurations();
 	}
 
 	public static EmailConfigurationReader getInstance() {
@@ -50,73 +58,44 @@ public class EmailConfigurationReader {
 		return instance;
 	}
 
-	/**
-	 * Read database config from resource and update default connection parameters.
-	 */
-	private void readConfig() {
-		Properties prop = new Properties();
-		String emailToListCommaSeparated = "";
+	private String getPropertyLogException(String propertyId) {
 		try {
-			prop = PropertiesFile.load(CONFIG_FILE);
-			smtpServer = prop.getProperty(EMAIL_SMTP_SERVER_TAG);
-			emailUser = prop.getProperty(EMAIL_USERNAME_TAG);
-			emailPassword = prop.getProperty(EMAIL_PASSWORD_TAG);
-			emailSender = prop.getProperty(EMAIL_SENDER_TAG);
-			emailEnabled = Boolean.parseBoolean(prop.getProperty(EMAIL_ENABLED));
-			emailToListCommaSeparated = prop.getProperty(EMAIL_TO);
-		} catch (IOException e) {
-			// Do nothing.
+			return getProperty(propertyId);
+		} catch (PropertyNotFoundException e) {
 			BiitLogger.errorMessage(this.getClass().getName(), e);
-		}
-
-		if (smtpServer == null || smtpServer.length() == 0) {
-			smtpServer = DEFAULT_EMAIL_SMTP_SERVER;
-		}
-
-		if (emailUser == null || emailUser.length() == 0) {
-			emailUser = DEFAULT_EMAIL_USERNAME;
-		}
-
-		if (emailPassword == null || emailPassword.length() == 0) {
-			emailPassword = DEFAULT_EMAIL_PASSWORD;
-		}
-
-		if (emailSender == null || emailSender.length() == 0) {
-			emailSender = DEFAULT_EMAIL_SENDER;
-		}
-
-		if (emailToList == null) {
-			emailToList = new ArrayList<>();
-			if (emailToListCommaSeparated != null && emailToListCommaSeparated.length() > 0) {
-				String[] users = emailToListCommaSeparated.split(",");
-				for (String user : users) {
-					emailToList.add(user.trim());
-				}
-			}
+			return null;
 		}
 	}
 
 	public synchronized String getSmtpServer() {
-		return smtpServer;
+		return getPropertyLogException(ID_EMAIL_SMTP_SERVER_TAG);
 	}
 
 	public synchronized String getEmailUser() {
-		return emailUser;
+		return getPropertyLogException(ID_EMAIL_USERNAME);
 	}
 
 	public synchronized String getEmailPassword() {
-		return emailPassword;
+		return getPropertyLogException(ID_EMAIL_PASSWORD);
 	}
 
 	public synchronized String getEmailSender() {
-		return emailSender;
+		return getPropertyLogException(ID_EMAIL_SENDER);
 	}
 
 	public boolean isEmailEnabled() {
-		return emailEnabled;
+		return Boolean.parseBoolean(getPropertyLogException(ID_EMAIL_ENABLED));
 	}
 
 	public List<String> getEmailToList() {
+		String emailToListCommaSeparated = getPropertyLogException(ID_EMAIL_TO);
+		List<String> emailToList = new ArrayList<>();
+		if (emailToListCommaSeparated != null && emailToListCommaSeparated.length() > 0) {
+			String[] users = emailToListCommaSeparated.split(",");
+			for (String user : users) {
+				emailToList.add(user.trim());
+			}
+		}
 		return emailToList;
 	}
 
